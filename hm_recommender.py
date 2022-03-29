ac6a52f909fc3cd9216009a21b0aab4b29b7885f
gnr_iwh <gnr@iwh-halle.de>
2022-03-29 16:33:15 +0200

    pictures update

--- hm_recommender.py
+++ hm_recommender.py
@@ -13,7 +13,7 @@ st.title('Get best style recommendations based on purchased baskets of millions 
 
 st.write('The purpose of this recommender is to show you the pieces that you may like based on the purchases of other people. The company sells more than 30,000 items and therefore you only see a small sample of products here. You can refresh the sample of clothes anytime you want')
 
-
+article_id_women_desc = pd.read_csv('article_id_women_desc.csv') # link between prod id and color and prod name
 df_sampled = pd.read_csv('df_sampled_42.csv') # create data framewith sampled data for recommendation
 sample_customer_pivot = df_sampled.pivot_table(index='invoice', columns=['article_id'], values='quantity').fillna(0) # make the table needed for recommendation
 
@@ -43,13 +43,20 @@ product = st.selectbox('Choose which product you like', single_articles) #select
 #product = np.random.choice(df_sampled['article_id'].unique()[0], 1)[0] #select one specific product
 
 st.write('The selected product is: ', product)
-product_recommended = list(get_recommendation(sample_customer_pivot, product).article_id.iloc[1:5]) # get the top 4 recommended products
-st.write('The recommender recommends those products')
-st.write('One', product_recommended[0])
-st.write('Two', product_recommended[1])
-st.write('Three', product_recommended[2])
-st.write('Four', product_recommended[3])
+product_recommended = list(get_recommendation(sample_customer_pivot, product).article_id.iloc[1:6]) # get the top 4 recommended products
 
+product_rec_name0 = article_id_women_desc[article_id_women_desc['article_id'] == product_recommended[0]].prod_color.unique()[0]
+product_rec_name1 = article_id_women_desc[article_id_women_desc['article_id'] == product_recommended[1]].prod_color.unique()[0]
+product_rec_name2 = article_id_women_desc[article_id_women_desc['article_id'] == product_recommended[2]].prod_color.unique()[0]
+product_rec_name3 = article_id_women_desc[article_id_women_desc['article_id'] == product_recommended[3]].prod_color.unique()[0]
+product_rec_name4 = article_id_women_desc[article_id_women_desc['article_id'] == product_recommended[4]].prod_color.unique()[0]
+
+st.write('The recommender recommends those products')
+st.write('One', product_rec_name0)
+st.write('Two', product_rec_name1)
+st.write('Three', product_rec_name2)
+st.write('Four', product_rec_name3)
+st.write('Four', product_rec_name4)
 
 #opening the image
 image1 = Image.open('pictures/opt_0153115039.jpg')
