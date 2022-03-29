11003aa979b1ab0135997c142bd4c6b2a91cea91
gnr_iwh <gnr@iwh-halle.de>
2022-03-29 10:06:21 +0200

    update

--- hm_recommender.py
+++ hm_recommender.py
@@ -7,4 +7,7 @@ import plotly.express as px
 # pip uninstall click
 # pip install click==8.0.4
 
-st.title('This is a nice recommender')
+st.title('This is a recommender')
+
+st.write('The purpose of this recommender is to show you the pieces that you may like based on the purchases of other people.')
+
