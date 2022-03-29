28a4dee5ed0e69713512809af0028e8cb990a879
gnr_iwh <gnr@iwh-halle.de>
2022-03-29 09:46:01 +0200

    update

--- hm_recommender.py
+++ hm_recommender.py
@@ -4,7 +4,7 @@ import streamlit as st
 import seaborn as sns
 import plotly.express as px
 
-pip uninstall click
-pip install click==8.0.4
+# pip uninstall click
+# pip install click==8.0.4
 
 st.title('This is a nice recommender')
